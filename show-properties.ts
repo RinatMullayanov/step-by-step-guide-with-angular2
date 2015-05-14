3040e52e557c06c1475fcdf302813df58bd81d72
Rinat <nutsgamedev@gmail.com>
2015-05-14 22:43:15 +0500

    add another component

---
+++ show-properties.ts
@@ -0,0 +1,19 @@
+/// <reference path="./typings/angular2/angular2.d.ts"/>
+import {Component, View, bootstrap, For} from 'angular2/angular2';
+
+@Component({
+  selector: 'display'
+})
+@View({
+  templateUrl: './display.html',
+  directives: [For]
+})
+class DisplayComponent {
+  myName: string;
+  names: string[];
+
+  constructor() {
+    this.myName = 'Alice';
+    this.names = ["Aarav", "Martín", "Shannon", "Ariana", "Kai"];
+  }
+}
